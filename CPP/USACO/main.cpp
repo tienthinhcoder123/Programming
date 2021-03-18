b964795146b39a8a09bc68c2e3e77e18097f9ba4
tienthinhcoder123 <thinhttfs91626@fpt.edu.vn>
2021-03-18 06:19:53 +0000

    CP

---
+++ CPP/USACO/main.cpp
@@ -0,0 +1,15 @@
+/*
+ID: your_id_here
+TASK: ...
+LANG: C++
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ofstream fout ("test.out");
+    ifstream fin ("test.in");
+    return 0;
+}
