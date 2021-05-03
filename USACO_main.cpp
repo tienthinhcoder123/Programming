fb6e1a716079cddc0a0894a8dd1ef9a15acac627
tienthinhcoder123 <79954343+tienthinhcoder123@users.noreply.github.com>
2021-05-03 11:29:18 +0700

    Add files via upload

---
+++ USACO_main.cpp
@@ -0,0 +1,20 @@
+/*
+ID: Tran Thinh [thinhtt1]
+TASK: test
+LANG: C++                 
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+	
+	ifstream cin("test.in");
+	ofstream cout("test.out");
+	
+	return 0;
+}
+
